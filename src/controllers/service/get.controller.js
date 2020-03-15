6f43439cbaf60217ddc237f1ef29f5c6a6e7529d
Levon Sargsyan <levonsargsyan1990@gmail.com>
2020-03-16 01:45:28 +0400

    Add JSDocs for controllers

--- src/controllers/service/get.controller.js
+++ src/controllers/service/get.controller.js
@@ -1,5 +1,13 @@
 import Service from '../../models/service.model';
 
+/**
+ * Finds the service by serviceId
+ *
+ * @param {Object} req - Request object
+ * @param {Object} req.params - Request params
+ * @param {string} req.params.serviceId - Id of service
+ * @param {Object} res - Response object
+ */
 export const get = async (req, res) => {
   const { user: organization } = req;
   const services = await Service.findOne({ organizationId: organization._id }).exec();
--- src/controllers/service/list.controller.js
+++ src/controllers/service/list.controller.js
@@ -1,8 +1,16 @@
 import Service from '../../models/service.model';
 
+
+/**
+ * Finds all services of the organization
+ *
+ * @param {Object} req - Request object
+ * @param {Object} req.user - Authenticated organization
+ * @param {Object} req.user._id - ID of organization
+ * @param {Object} res - Response object
+ */
 export const list = async (req, res, next) => {
   const { user: organization } = req;
   const services = await Service.find({ organizationId: organization._id }).exec();
-  console.log('List services');
-  console.log(res.send(services));
-}
+  res.send(services);
+};
