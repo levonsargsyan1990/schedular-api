c39b4024573a55b161906413fbed92f644e69fe9
Levon Sargsyan <levonsargsyan1990@gmail.com>
2020-03-16 19:19:37 +0400

    Add booking model

---
+++ src/models/booking.modal.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const locationSchema = new mongoose.Schema({
+  address: {
+    type: String,
+    default: '',
+  },
+  long: {
+    type: Number,
+    required: true,
+  },
+  lat: {
+    type: Number,
+    required: true,
+  },
+});
+
+const schema = new mongoose.Schema({
+  organizationId: {
+    type: ObjectId,
+    required: true,
+  },
+  serviceId: {
+    type: ObjectId,
+    required: true,
+  },
+  providerId: {
+    type: ObjectId,
+    required: true,
+  },
+  startsAt: {
+    type: Date,
+    required: true,
+  },
+  endsAt: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: locationSchema,
+  },
+}, { timestamps: true });
+
+const Booking = mongoose.model('Booking', schema);
+
+export default Booking;
